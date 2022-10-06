7627b12a4c2754657f657f25a2e1735b41ae352e
Leon Kempen <l.m.kempen@outlook.com>
2022-10-06 10:10:21 +0200

    Implemented pseudo code naively

--- UniXcoder/AST_to_sequence.py
+++ UniXcoder/AST_to_sequence.py
@@ -3,4 +3,15 @@ from tree_sitter import Tree, Node
 
 def one_to_one(root_node: Tree):
     # implenment the algorithm from unixcoder here
-    pass
+    sequence = ""
+    name = root_node.text
+
+    # Is Leaf
+    if root_node.child_count == 0:
+        sequence += name
+    else:
+        sequence += name + "::left"
+        for node in root_node.children:
+            sequence += one_to_one(node)
+        sequence += name + "::right"
+    return sequence
